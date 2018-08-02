4f5ebab40431343095f119820b7b8ea817197d47
Julian Rüth <julian.rueth@fsfe.org>
2018-08-02 02:33:49 +0200

    We do not install cddlib prefixed

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CPPFLAGS="-I$PREFIX/include -DDISABLE_COMMENTATOR $CPPFLAGS"
+export CPPFLAGS="-I$PREFIX/include -DNOCDDPREFIX -DDISABLE_COMMENTATOR $CPPFLAGS"
 export LDFLAGS="-L$PREFIX/lib -lcddgmp -lgmp -lm $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export CFLAGS="-O2 -g -fPIC $CFLAGS"
-export CXXFLAGS="-I$PREFIX/include -O2 -g -fPIC $CXXFLAGS"
+export CXXFLAGS="-I$PREFIX/include -DNOCDDPREFIX -O2 -g -fPIC $CXXFLAGS"
 
 make
 mkdir -p "$PREFIX/bin"
