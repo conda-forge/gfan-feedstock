e8020cbb7e01bb24cb632fecfe9ee9409660cd39
Julian Rüth <julian.rueth@fsfe.org>
2018-08-02 02:13:24 +0200

    Pass -I to g++ compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ export CPPFLAGS="-I$PREFIX/include -DDISABLE_COMMENTATOR $CPPFLAGS"
 export LDFLAGS="-L$PREFIX/lib -lcddgmp -lgmp -lm $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export CFLAGS="-O2 -g -fPIC $CFLAGS"
-export CXXFLAGS="-O2 -g -fPIC $CXXFLAGS"
+export CXXFLAGS="-I$PREFIX/include -O2 -g -fPIC $CXXFLAGS"
 
 make
 mkdir -p "$PREFIX/bin"
