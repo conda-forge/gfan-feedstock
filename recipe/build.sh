c848fbacbd9363ae9e2ec30b63c7ef156b5cc0a7
Julian Rüth <julian.rueth@fsfe.org>
2018-08-03 01:10:20 +0200

    Add std=c++0x to CXXFLAGS
    so we can use the auto keyword

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ export CPPFLAGS="-I$PREFIX/include -DNOCDDPREFIX -DDISABLE_COMMENTATOR $CPPFLAGS
 export LDFLAGS="-L$PREFIX/lib -lcddgmp -lgmp -lm $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export CFLAGS="-O2 -g -fPIC $CFLAGS"
-export CXXFLAGS="-I$PREFIX/include -DNOCDDPREFIX -O2 -g -fPIC $CXXFLAGS"
+export CXXFLAGS="-I$PREFIX/include -DNOCDDPREFIX -O2 -g -fPIC -std=c++0x $CXXFLAGS"
 
 make
 mkdir -p "$PREFIX/bin"
