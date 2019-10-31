62d5e97673764d5a4c82c13923a1783d422975f8
Isuru Fernando <isuruf@gmail.com>
2019-10-31 01:02:58 -0500

    clean up build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-export CPPFLAGS="-I$PREFIX/include -DNOCDDPREFIX -DDISABLE_COMMENTATOR $CPPFLAGS"
-export LDFLAGS="-L$PREFIX/lib -lcddgmp -lgmp -lm $LDFLAGS"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export CFLAGS="-O2 -g -fPIC $CFLAGS"
-export CXXFLAGS="-I$PREFIX/include -DNOCDDPREFIX -O2 -g -fPIC -std=c++0x $CXXFLAGS"
+export CPPFLAGS="-DNOCDDPREFIX -DDISABLE_COMMENTATOR $CPPFLAGS"
+export LDFLAGS="$LDFLAGS -lcddgmp -lgmp -lm"
+export CFLAGS="-g -fPIC $CFLAGS"
+export CXXFLAGS="-DNOCDDPREFIX -g -fPIC $CXXFLAGS"
 
-make
+make -j${CPU_COUNT}
 mkdir -p "$PREFIX/bin"
 cp -pf gfan "$PREFIX/bin/"
 cd "$PREFIX/bin"
