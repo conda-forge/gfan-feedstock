2f9fd3ea9111b8ef59078932bdc8c70cbee8260b
Isuru Fernando <isuruf@gmail.com>
2020-01-02 09:03:12 +0530

    Remove log2

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ export LDFLAGS="$LDFLAGS -lcddgmp -lgmp -lm"
 export CFLAGS="-g -fPIC $CFLAGS"
 export CXXFLAGS="-DNOCDDPREFIX -g -fPIC $CXXFLAGS"
 
+find ${SRC_DIR} -type f -print0 | xargs -0 sed -i "s/log2/logger2/g"
+
 make -j${CPU_COUNT}
 mkdir -p "$PREFIX/bin"
 cp -pf gfan "$PREFIX/bin/"
