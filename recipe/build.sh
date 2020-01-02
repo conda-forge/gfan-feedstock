7b39e0160cba8fd1f8988cb244eb6310cb7a2c56
Isuru Fernando <isuruf@gmail.com>
2020-01-02 09:26:30 +0530

    Replace inside src

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export CFLAGS="-g -fPIC $CFLAGS"
 export CXXFLAGS="-DNOCDDPREFIX -g -fPIC $CXXFLAGS"
 
 if [[ "$target_platform" == "osx-64" ]]; then
-  find ${SRC_DIR} -type f -print0 | xargs -0 sed -i '' "s/log2/logger2/g"
+  find ${SRC_DIR}/src -type f -print0 | xargs -0 sed -i '' "s/log2/logger2/g"
 fi
 
 make -j${CPU_COUNT}
