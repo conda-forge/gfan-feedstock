6cd0f071d2c4b7a3d492de0c230858806928a2c1
Isuru Fernando <isuruf@gmail.com>
2020-01-02 09:18:59 +0530

    Fix sed command

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,9 @@ export LDFLAGS="$LDFLAGS -lcddgmp -lgmp -lm"
 export CFLAGS="-g -fPIC $CFLAGS"
 export CXXFLAGS="-DNOCDDPREFIX -g -fPIC $CXXFLAGS"
 
-find ${SRC_DIR} -type f -print0 | xargs -0 sed -i "s/log2/logger2/g"
+if [[ "$target_platform" == "osx-64" ]]; then
+  find ${SRC_DIR} -type f -print0 | xargs -0 sed -i '' "s/log2/logger2/g"
+fi
 
 make -j${CPU_COUNT}
 mkdir -p "$PREFIX/bin"
